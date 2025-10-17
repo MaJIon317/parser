de01563cb84103df57c18b4233bcbbbcd5099e15
Your Name <you@example.com>
2025-10-18 01:43:34 +0300

    DF

--- app/Filament/Widgets/ProductWidget.php
+++ app/Filament/Widgets/ProductWidget.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\DB;
 
 class ProductWidget extends StatsOverviewWidget
 {
+    public function getColumns(): int | array
+    {
+        return 5;
+    }
+
     protected function getStats(): array
     {
         return [
@@ -20,12 +25,16 @@ class ProductWidget extends StatsOverviewWidget
                 value: Product::where('price', '>', 0)->count(),
             ),
             StatsOverviewWidget\Stat::make(
+                label: 'Active products',
+                value: Product::where('status', 'active')->count(),
+            ),
+            StatsOverviewWidget\Stat::make(
                 label: 'Products with details',
                 value: Product::whereNotNull('detail')->where('detail', '!=', '[]')->count(),
             ),
             StatsOverviewWidget\Stat::make(
-                label: 'All Products',
-                value: Product::where('status', 'active')->count(),
+                label: 'Processed today',
+                value: Product::whereDate('last_parsing', '>=', now()->subDay())->count(),
             ),
         ];
     }
